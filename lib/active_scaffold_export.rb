e902b09801a1ef6313d6c87a725bdd9a8a0c08e9
vhochstein <v.hochstein@highstone.de>
2011-11-23 10:56:41 +0100

    remove code to copy assets to public folder

--- lib/active_scaffold_export.rb
+++ lib/active_scaffold_export.rb
@@ -24,16 +24,3 @@ module ActiveScaffold
 end
 
 ActionView::Base.send(:include, ActiveScaffold::Helpers::ExportHelpers)
-
-
-##
-## Run the install assets script, too, just to make sure
-## But at least rescue the action in production
-##
-Rails::Application.initializer("active_scaffold_export.install_assets", :after => "active_scaffold.install_assets") do
-  begin
-    ActiveScaffoldAssets.copy_to_public(ActiveScaffoldExport.root)
-  rescue
-    raise $! unless Rails.env == 'production'
-  end
-end if defined?(ACTIVE_SCAFFOLD_EXPORT_GEM)
